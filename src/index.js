e48696e685eeaa71a91f2bae3c649c628fd8e1ec
hemiahwu <309595700@qq.com>
2022-02-07 21:00:56 +0800

    05

--- src/index.js
+++ src/index.js
@@ -7,32 +7,36 @@ import './index.css'
 function Greeting() {
   return (
     <section className="booklist">
-      <Book />
+      <Book job="developer" />
+      <Book title="你看见的未来" number="22" />
+      <Book title="他看见的未来" />
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props)
+  const title = '我看见的未来!!'
+  const author = '龙树谅'
+  const image =
+    'https://img10.360buyimg.com/n1/s200x200_jfs/t1/119522/15/20383/219156/61f20c1aEb50cbad5/5eeaffe8b9873f9d.jpg'
+  const authorStyle = {
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '0.25rem',
+  }
+
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={image} alt="" />
+      <h1>{title}</h1>
+      <h4 style={authorStyle}>{author}</h4>
+
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
     </article>
   )
 }
-
-const Image = () => (
-  <img
-    src="https://img10.360buyimg.com/n1/s200x200_jfs/t1/119522/15/20383/219156/61f20c1aEb50cbad5/5eeaffe8b9873f9d.jpg"
-    alt=""
-  />
-)
-const Title = () => <h1>我看见的未来</h1>
-const Author = () => (
-  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-    龙树谅
-  </h4>
-)
 
 ReactDom.render(<Greeting />, document.getElementById('root'))
