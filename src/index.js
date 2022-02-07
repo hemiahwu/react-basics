61f2ebba6be2bbf174fb3517f18e746d36a8fde5
hemiahwu <309595700@qq.com>
2022-02-07 20:52:27 +0800

    04

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+// CSS
+import './index.css'
+
 function Greeting() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section className="booklist">
+      <Book />
+    </section>
   )
 }
 
-const Person = () => <h2>米斯特吴</h2>
-const Message = () => <p>hello world</p>
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+const Image = () => (
+  <img
+    src="https://img10.360buyimg.com/n1/s200x200_jfs/t1/119522/15/20383/219156/61f20c1aEb50cbad5/5eeaffe8b9873f9d.jpg"
+    alt=""
+  />
+)
+const Title = () => <h1>我看见的未来</h1>
+const Author = () => (
+  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
+    龙树谅
+  </h4>
+)
 
 ReactDom.render(<Greeting />, document.getElementById('root'))
