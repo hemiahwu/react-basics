196237bfbd6f5af5d2ab721a252e5d84b5b1164c
hemiahwu <309595700@qq.com>
2022-02-07 21:34:27 +0800

    07

--- src/index.js
+++ src/index.js
@@ -11,65 +11,62 @@ const authorStyle = {
 }
 
 // 重构数据
-const firstBook = {
-  img:
-    'https://img10.360buyimg.com/n1/s200x200_jfs/t1/119522/15/20383/219156/61f20c1aEb50cbad5/5eeaffe8b9873f9d.jpg',
-  title: '我看见的未来!!',
-  author: '龙树谅',
-}
-
-const secondBook = {
-  img:
-    'https://img10.360buyimg.com/n1/s200x200_jfs/t1/119522/15/20383/219156/61f20c1aEb50cbad5/5eeaffe8b9873f9d.jpg',
-  title: '你看见的未来',
-  author: '吴树谅',
-}
-
-const thirdBook = {
-  img:
-    'https://img10.360buyimg.com/n1/s200x200_jfs/t1/119522/15/20383/219156/61f20c1aEb50cbad5/5eeaffe8b9873f9d.jpg',
-  title: '他看见的未来',
-  author: '海树谅',
-}
+const books = [
+  {
+    id: 1,
+    img:
+      'https://img10.360buyimg.com/n1/s200x200_jfs/t1/119522/15/20383/219156/61f20c1aEb50cbad5/5eeaffe8b9873f9d.jpg',
+    title: '我看见的未来!!',
+    author: '龙树谅',
+  },
+  {
+    id: 2,
+    img:
+      'https://img10.360buyimg.com/n1/s200x200_jfs/t1/119522/15/20383/219156/61f20c1aEb50cbad5/5eeaffe8b9873f9d.jpg',
+    title: '你看见的未来',
+    author: '吴树谅',
+  },
+  {
+    id: 3,
+    img:
+      'https://img10.360buyimg.com/n1/s200x200_jfs/t1/119522/15/20383/219156/61f20c1aEb50cbad5/5eeaffe8b9873f9d.jpg',
+    title: '他看见的未来',
+    author: '海树谅',
+  },
+]
 
 function Greeting() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At aliquid
-          doloribus accusamus quibusdam molestiae fugiat beatae praesentium sunt
-          dolorem maxime.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-      <Book
-        img={thirdBook.img}
-        title={thirdBook.title}
-        author={thirdBook.author}
-      />
+      {books.map((book, index) => {
+        return <Book key={book.id} book={book} />
+      })}
     </section>
   )
 }
 
+const handleClick = (e) => {
+  console.log(e)
+  console.log(e.target)
+}
+
+const passParams = (author) => {
+  console.log(author)
+}
+
 const Book = (props) => {
-  const { img, title, author, children } = props
-  console.log(props)
+  const { img, title, author, children } = props.book
 
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4 style={authorStyle}>{author}</h4>
       {children}
+
+      <button onClick={handleClick}>按钮</button>
+
+      <button onClick={() => passParams(author)}>传参</button>
     </article>
   )
 }
